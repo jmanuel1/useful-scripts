e8609a9d20453ff188255dce980eae64c0ca7515
Jason Manuel <jama.indo@hotmail.com>
2020-02-15 15:44:44 -0700

    Add a script to delete print jobs on Windows
    I assume it works when given administrative permissions.

---
+++ src/windows/delete-print-jobs.cmd
@@ -0,0 +1,6 @@
+rem NOTE: These commands require administrator access.
+
+net stop spooler
+del %systemroot%\system32\spool\printers\*.shd
+del %systemroot%\system32\spool\printers\*.spl
+net start spooler
